10a9cc49f4a154682749fd6f2300686896ada62a
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-15 23:42:33 +0530

    :sparkles: Trim and clean commands

--- index.spec.ts
+++ index.spec.ts
@@ -3,7 +3,7 @@ import { puppet } from "./index";
 describe("puppet basic run", () => {
   it("downloads and runs URL", async () => {
     const result = await puppet("https://pastebin.com/raw/bqRmm1z9");
-    expect(result).toBeUndefined();
+    expect(result).toBeTruthy();
   });
   it("runs array of commands", async () => {
     const result = await puppet([
@@ -11,15 +11,29 @@ describe("puppet basic run", () => {
       "take a screenshot",
       "save to example.png",
     ]);
-    expect(result).toBeUndefined();
+    expect(result).toBeTruthy();
   });
   it("runs a file path", async () => {
     const result = await puppet("examples/screenshot.txt");
-    expect(result).toBeUndefined();
+    expect(result).toBeTruthy();
   });
   it("throws if no commands", async () => {
     expect((puppet as any)()).rejects.toEqual(
       new Error("Argument must be a string or an array of strings")
     );
+  });
+});
+
+describe("cleans commands", () => {
+  it("trims commands", async () => {
+    const result = await puppet([
+      "go to example.com   ",
+      "   take a screenshot",
+    ]);
+    expect(result.commands).toEqual(["go to example.com", "take a screenshot"]);
+  });
+  it("removes empty commands", async () => {
+    const result = await puppet(["go to example.com", "take a screenshot", ""]);
+    expect(result.commands).toEqual(["go to example.com", "take a screenshot"]);
   });
 });
--- index.ts
+++ index.ts
@@ -28,6 +28,24 @@ export const puppet = async (commandsOrFile: string[] | string) => {
   throw new Error("Argument must be a string or an array of strings");
 };
 
+/**
+ * Runs Puppet commands
+ * @param commands - Commands to run
+ */
 const _puppet = async (commands: string[]) => {
+  commands = commands.map((i) => i.trim()).filter((i) => i);
   log("Starting Puppet");
+  const browser = await launch();
+  const page = await browser.newPage();
+  for await (const command of commands) {
+    await _command(command, page);
+  }
+  const result = { commands, url: page.url() };
+  await browser.close();
+  return result;
+};
+_puppet(["open example.com"]).then((r) => console.log(r));
+
+const _command = async (command: string, page: Page) => {
+  return page;
 };
