2379c2fb821a8c38c03408b5e3385c483515fd77
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-15 23:27:33 +0530

    :white_check_mark: Add tests for function

--- index.spec.ts
+++ index.spec.ts
@@ -1,7 +1,25 @@
-import { nodeTs } from "./index";
+import { puppet } from "./index";
 
-describe("puppet", () => {
-  it("works", () => {
-    expect(nodeTs()).toBeTruthy();
+describe("puppet basic run", () => {
+  it("downloads and runs URL", async () => {
+    const result = await puppet("https://pastebin.com/raw/bqRmm1z9");
+    expect(result).toBeUndefined();
+  });
+  it("runs array of commands", async () => {
+    const result = await puppet([
+      "go to example.com",
+      "take a screenshot",
+      "save to example.png",
+    ]);
+    expect(result).toBeUndefined();
+  });
+  it("runs a file path", async () => {
+    const result = await puppet("examples/screenshot.txt");
+    expect(result).toBeUndefined();
+  });
+  it("throws if no commands", async () => {
+    expect((puppet as any)()).rejects.toEqual(
+      new Error("Argument must be a string or an array of strings")
+    );
   });
 });
--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { launch, Page } from "puppeteer";
 import { log } from "signale";
 import got from "got";
-import {} from "fs-extra";
+import { readFile } from "fs-extra";
+import { join } from "path";
 
 /**
  *
@@ -12,19 +13,19 @@ import {} from "fs-extra";
  */
 export const puppet = async (commandsOrFile: string[] | string) => {
   if (typeof commandsOrFile === "string") {
-    return puppetFile(commandsOrFile);
+    if (
+      commandsOrFile.startsWith("https://") ||
+      commandsOrFile.startsWith("http://")
+    ) {
+      const commands = await got.get(commandsOrFile);
+      return _puppet(commands.body.split("\n"));
+    }
+    const commands = await readFile(join(".", commandsOrFile), "utf8");
+    return _puppet(commands.split("\n"));
   } else if (Array.isArray(commandsOrFile)) {
     return _puppet(commandsOrFile);
   }
   throw new Error("Argument must be a string or an array of strings");
-};
-
-const puppetFile = async (file: string) => {
-  if (file.startsWith("https://") || file.startsWith("http://")) {
-    const commands = await got.get(file);
-    return _puppet(commands.body.split("\n"));
-  }
-  console.log("Starting Puppet");
 };
 
 const _puppet = async (commands: string[]) => {
