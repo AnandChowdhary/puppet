2493436bef9ea5111136823ea5f7cb79214a43dc
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-15 22:33:10 +0530

    :heavy_plus_sign: Add got to fetch file

--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
 import { launch, Page } from "puppeteer";
+import { log } from "signale";
+import got from "got";
 
-export const puppet = async (commands: string[] | string) => {
-  console.log("Starting Puppet");
+/**
+ *
+ * @param commandsOrFile - Commands for Pupper or a file path/URL
+ * @example puppet("path/to/commands.puppet")
+ * @example puppet("https://example.com/commands.puppet")
+ * @example puppet(["go to example.com", "download page as PDF"])
+ */
+export const puppet = async (commandsOrFile: string[] | string) => {
+  if (typeof commandsOrFile === "string") {
+    return puppetFile(commandsOrFile);
+  } else if (Array.isArray(commandsOrFile)) {
+    return _puppet(commandsOrFile);
+  }
+  throw new Error("Argument must be a string or an array of strings");
 };
 
-export const puppetFile = async (file: string) => {
+const puppetFile = async (file: string) => {
+  if (file.startsWith("https://") || file.startsWith("http://")) {
+    //
+  }
   console.log("Starting Puppet");
 };
 
 const _puppet = async (commands: string[]) => {
-  //
+  log("Starting Puppet");
 };
